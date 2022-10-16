e0e85fde09dc511fa0b4f06b2082dcac3901f7f9
MaxPankratov <maxpankratov04@gmail.com>
2022-10-16 13:50:20 +0300

    first and last

---
+++ 1.py
@@ -0,0 +1,19 @@
+def convert(number, base):
+	result = ''
+
+	while number > 0:
+		result = str(number % base) + result
+		number //= base
+	return result
+
+
+enteredNumber = int(input("Введите число: "))
+
+while True:	
+	enteredBase = int(input("Введите основание целевой системы счисления (2 или 8, например): "))
+	if (2 <= enteredBase <= 9):
+		break
+	print("!!! Основание системы счисления должно быть от 2 до 9 !!!")
+
+
+print("Вывод:", enteredNumber, "->", convert(enteredNumber, enteredBase))
